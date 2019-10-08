254dc2e5f7c2cb7f0fba517672603a5a8d6e2b1f
Michelle Wong <michellekwong2@gmail.com>
2019-10-08 15:37:55 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(given)
+    @name = given 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def job=(given)
+    @job = given
+  end 
+  
+  def job 
+    @job
+  end
+end
